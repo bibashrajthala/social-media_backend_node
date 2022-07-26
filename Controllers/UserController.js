f847c225abc4954b8fbfad77d184b8419df69671
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-27 03:32:53 +0545

    implement user update

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+
 import UserModel from "../Models/userModel.js";
 
 // get a user
@@ -6,7 +8,7 @@ export const getUser = async (req, res) => {
 
   try {
     const user = await UserModel.findById(id);
-    console.log("hello");
+    // console.log("hello");
 
     // when user is found, ie id of url params and our database matches
     if (user) {
@@ -20,5 +22,35 @@ export const getUser = async (req, res) => {
   } catch (error) {
     res.status(404).json("User doesnot exists!");
     console.log("error:", error.message);
+  }
+};
+
+// edit / update a user
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { currentUserId, currentUserIsAdminStatus, password } = req.body;
+
+  // if the user is admin or if he is id entered matches to the user's id in database then only he can update the user
+  if (currentUserIsAdminStatus || id === currentUserId) {
+    try {
+      // user also wants to update password we need to hash that password before updating, so before updating our databse we hashed it and assigned it to req.body.password because we are updating to the req.body object(ie to what user entered)
+      if (password) {
+        const saltRound = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, saltRound);
+        req.body.password = hashedPassword;
+      }
+
+      // find the user with id in params and update it with what user entered , update database to the new data=> new:true
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+
+      res.status(200).json(user);
+      // console.log("user is updated");
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  } else {
+    res.status(403).json("Access Denied! You can only edit your own account.");
   }
 };
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUser } from "../Controllers/UserController.js";
+import { getUser, updateUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get("/", (_, res) => {
 });
 
 router.get("/:id", getUser);
+router.put("/:id", updateUser);
 
 export default router;
