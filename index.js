f3010de947d8b4cd71badb60f1d69a8582defdbd
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-26 23:53:21 +0545

    download dependencies and setup server

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import AuthRoute from "./routes/AuthRoute.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(port, (req, res) => {
+      console.log(`Listening to port ${port}`);
+    })
+  )
+  .catch((error) => console.log(error));
+
+app.get("/", (_, res) => {
+  res.send("Sever is running");
+});
+
+app.use("/auth", AuthRoute);
+
+export default app;
