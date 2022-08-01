c2807722e7530e3fd2052e269db26cea12d6f9d6
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-08-01 15:21:28 +0545

    improve jwt validation

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -8,16 +8,17 @@ const authMiddleWare = async (req, res, next) => {
     // take the token from the header's authorization
     // const {bearer,token} = req.headers.authorization.split(" ") // or
     const token = req.headers.authorization.split(" ")[1];
-    console.log(token);
+    // console.log(token);
     if (token) {
       // only if token is present then verify
       const decoded = jwt.verify(token, secretKey); // verify token based on our secret key
-      console.log(decoded);
+      // console.log(decoded, "decoded data");
       req.body._id = decoded?.id; // some requests should be allowed only when decoded id is equal to user id,which is only possible when token is verified so replace user's id with decoded id and, for verified token it will still work and if not verified it will give error
     }
     next(); // go to next function of the route
   } catch (error) {
-    console.log(error);
+    console.log({ message: error.message }, "jwt validity error");
+    res.status(500).send("jwt validity expired");
   }
 };
 
